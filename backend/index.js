5b9467516cc7467d8f90c4f82613e399c0e8657d
Riddhi <ridhirani54@gmail.com>
2026-05-13 14:28:29 +0530

    fix deploy cors

--- backend/index.js
+++ backend/index.js
@@ -99,7 +99,7 @@ socket.on("joinGroup", (groupId) => {
 });
 
 app.use(cors({
-  origin: ["https://cheery-stroopwafel-de0527.netlify.app", "http://localhost:5173", process.env.CLIENT_URL],
+  origin: ["https://indigenous-social-media-platform.onrender.com", "http://localhost:5173", process.env.CLIENT_URL],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Cookie", "Set-Cookie"],
